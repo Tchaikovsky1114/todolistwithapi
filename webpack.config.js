60b30bfb7bc39ccc2a2a3bbb16ddf3b6f3179e44
Kim Myungseong <forwarm5891@gmail.com>
2022-05-17 17:45:55 +0900

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const HtmlPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
