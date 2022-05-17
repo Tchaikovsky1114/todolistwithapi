3481bdda581d1b394f5b3c126fbeb5c9b04e1050
Kim Myungseong <forwarm5891@gmail.com>
2022-05-17 20:00:35 +0900

    fix: fix entry point

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -4,7 +4,7 @@ const CopyPlugin = require('copy-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
   mode: 'production',
-  entry: 'index.js',
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
