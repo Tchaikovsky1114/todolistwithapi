02085b845474d21ab09efd170fc7486396a10a64
Kim Myungseong <forwarm5891@gmail.com>
2022-05-17 17:47:00 +0900

    fix: fix module entry in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 module.exports = {
   mode: 'production',
-  entry: './src/index.js',
+  entry: 'src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
